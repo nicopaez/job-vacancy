9d0afd7302da7b294bc726d26ad5ae43888ba0ca
matlock <jfflores90@gmail.com>
2015-06-06 16:26:47 -0300

    se agregan los features con el tag @wip antes de empezar a trabajar

---
+++ features/step_definitions/publish_expired_offers.rb
@@ -0,0 +1,27 @@
+Given(/^an expired job named "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a post "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^accept the action$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^"(.*?)" is updated$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^an expired offer named "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^an invalid "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^an error of "(.*?)" it's shown$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
