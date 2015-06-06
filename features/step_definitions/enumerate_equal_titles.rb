e9bd60ba508eefa45576e23f73d1e7aca67ea8da
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-06-06 17:31:31 -0300

    Adding enumerate_equal_titles steps - all with #pending

---
+++ features/step_definitions/enumerate_equal_titles.rb
@@ -0,0 +1,7 @@
+Given(/^an offer with the title "(.*?)"$/) do |a_title|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill the title of a new offer with "(.*?)" and submit$/) do |a_title|
+  pending # express the regexp above with the code you wish you had
+end
