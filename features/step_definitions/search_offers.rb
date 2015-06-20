a18d3d0032f4a15bb317887a2f0f8595352fc1e7
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-06-19 21:07:16 -0300

    US08 - Adding features and pending steps

---
+++ features/step_definitions/search_offers.rb
@@ -0,0 +1,35 @@
+Given(/^there is a job offer with title 'Programmer' and description 'Good job'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^there is a job offer with title 'Java' and description 'Nice job'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I write 'Good job' on the search bar and press search$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the offer with description 'Good job'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I write 'Excelent job' on the search bar and press search$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should't see any offers$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I write 'Programmer' on the search bar and press search$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the offer with title 'Programmer'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I write 'Tester' on the search bar and press search$/) do
+  pending # express the regexp above with the code you wish you had
+end
