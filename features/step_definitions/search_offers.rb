1fad34f3dd597bdf15320eca9ac88a817d903cb0
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-06-19 22:25:42 -0300

    US08 - Changed the quotes symbol for double quotes + changed the step definitions

--- features/step_definitions/search_offers.rb
+++ features/step_definitions/search_offers.rb
@@ -1,20 +1,12 @@
-Given(/^there is a job offer with title 'Programmer' and description 'Good job'$/) do
+Given(/^there is a job offer with title "(.*?)" and description "(.*?)"$/) do |arg1, arg2|
   pending # express the regexp above with the code you wish you had
 end
 
-Given(/^there is a job offer with title 'Java' and description 'Nice job'$/) do
+Given(/^I write "(.*?)" on the search bar and press search$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Given(/^I write 'Good job' on the search bar and press search$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I should see the offer with description 'Good job'$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Given(/^I write 'Excelent job' on the search bar and press search$/) do
+Then(/^I should see the offer with description "(.*?)"$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
@@ -22,14 +14,7 @@ Then(/^I should't see any offers$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-Given(/^I write 'Programmer' on the search bar and press search$/) do
+Then(/^I should see the offer with title "(.*?)"$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Then(/^I should see the offer with title 'Programmer'$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Given(/^I write 'Tester' on the search bar and press search$/) do
-  pending # express the regexp above with the code you wish you had
-end
