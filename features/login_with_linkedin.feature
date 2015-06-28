f28b0c0388b2686fcf7845966de90e471cb2a814
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-06-27 22:41:14 -0300

    US14 : Added features and pending steps

---
+++ features/login_with_linkedin.feature
@@ -0,0 +1,18 @@
+@wip
+Feature: Users can log in using their Linkedin accounts
+
+#  @wip
+#  Background:
+#    Given I access the login page
+#    Given I press the 'login with Linkedin button'
+#    Given I press LinkedIn's authentication dialog box
+#
+#  @wip
+#  Scenario: A user can log in with his Linkedin account
+#    Given I write the email "test@email.com" and password "password"
+#    Then I should see the email "test@email.com" on the webpage
+#
+#  @wip
+#  Scenario: A user can't log in with an Linkedin account
+#    Given I write the email "test2@email.com" and password "password2"
+#    Then I should not see the email "test2@email.com" on the webpage
---
+++ features/step_definitions/login_with_linkedin.rb
@@ -0,0 +1,23 @@
+Given(/^I access the login page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I press the 'login with Linkedin button'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I press LinkedIn's authentication dialog box$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I write the email "(.*?)" and password "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the email "(.*?)" on the webpage$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not see the email "(.*?)" on the webpage$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
