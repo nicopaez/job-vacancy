ce72c0d0aaad8dba413828f629a73e2ed49485db
“camilagarcia113” <camilagarcia.113@gmail.com>
2015-07-01 19:26:45 -0300

    US14 - Adding pending files in order to fix travis

---
+++ app/models/account.rb
@@ -0,0 +1,11 @@
+class Account
+  include DataMapper::Resource
+
+  # property <name>, <type>
+  property :id, Serial
+  property :name, String
+  property :email, String
+  property :role, String
+  property :uid, String
+  property :provider, String
+end
